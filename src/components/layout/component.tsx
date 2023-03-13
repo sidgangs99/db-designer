2faf6b58140c3dbd21105a4fe02824eac29c6cbf
Siddharth Gangwar <siddharth_gangwar@cargill.com>
2023-03-13 23:44:58 +0530

    chore(react-split): to split screen into two parts
    https://www.npmjs.com/package/react-split

--- src/components/layout/component.tsx
+++ src/components/layout/component.tsx
@@ -1,5 +1,31 @@
+import Split from 'react-split';
+
 import { ILayoutComponentProps } from './types';
 
 export default function LayoutComponent(props: ILayoutComponentProps) {
-    return <></>;
+    function C1() {
+        return <div className="">Component 1 {Date.now()}</div>;
+    }
+    function C2() {
+        return <div className="">Component 2 {Date.now()}</div>;
+    }
+    return (
+        <main className="h-full w-full border border-black">
+            <Split
+                className="flex"
+                sizes={[30, 70]}
+                minSize={300}
+                expandToMin={false}
+                gutterSize={10}
+                gutterAlign="center"
+                snapOffset={30}
+                dragInterval={6}
+                direction="horizontal"
+                cursor="col-resize"
+            >
+                <C1 />
+                <C2 />
+            </Split>
+        </main>
+    );
 }
