db032eb537b3159d30017000a5064f2f8e20b59a
Siddharth Gangwar <sidgangs99@gmail.com>
2023-03-14 13:05:06 +0530

    chore(split.js): replace react-split with split.js
    https://www.npmjs.com/package/split.js?utm_source=cdnjs&utm_medium=cdnjs_link&utm_campaign=cdnjs_library#usage-examples

--- src/components/layout/component.tsx
+++ src/components/layout/component.tsx
@@ -1,31 +1,60 @@
-import Split from 'react-split';
-
+import { useEffect, useRef } from 'react';
+import Split from 'split.js';
 import { ILayoutComponentProps } from './types';
 
 export default function LayoutComponent(props: ILayoutComponentProps) {
     function C1() {
-        return <div className="">Component 1 {Date.now()}</div>;
+        return (
+            <div className="border border-red-400">
+                Component 1 {Date.now()}
+            </div>
+        );
     }
     function C2() {
-        return <div className="">Component 2 {Date.now()}</div>;
+        return (
+            <div className="border border-black">Component 2 {Date.now()}</div>
+        );
     }
+
+    const splitContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (splitContainer.current) {
+            Split(['#left-pane', '#right-pane'], {
+                sizes: [80, 200],
+                minSize: 400,
+                gutterSize: 5,
+                direction: 'horizontal'
+            });
+        }
+    }, []);
+
     return (
-        <main className="h-full w-full border border-black">
-            <Split
-                className="flex"
-                sizes={[30, 70]}
-                minSize={300}
-                expandToMin={false}
-                gutterSize={10}
-                gutterAlign="center"
-                snapOffset={30}
-                dragInterval={6}
-                direction="horizontal"
-                cursor="col-resize"
-            >
+        <div
+            className="flex w-full border border-green-400"
+            ref={splitContainer}
+        >
+            <div id="left-pane">
                 <C1 />
+            </div>
+            <div id="right-pane">
                 <C2 />
-            </Split>
-        </main>
+            </div>
+        </div>
     );
 }
+
+// return (
+//     <Split
+//         className="split-container"
+//         sizes={[50, 50]}
+//         minSize={100}
+//         gutterSize={10}
+//         direction="horizontal"
+//         // onDragEnd={handleResize}
+//     >
+//         <C1 />
+//         <C2 />
+//     </Split>
+// );
+// }
