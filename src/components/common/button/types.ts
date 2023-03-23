ede76da58bbc786b95d07fc3403140a55337fc86
Siddharth Gangwar <sidgangs99@gmail.com>
2023-03-23 11:43:34 +0530

    fix(visualise): primary node connection migrate from custom state to reactflow

--- src/components/common/button/types.ts
+++ src/components/common/button/types.ts
@@ -4,6 +4,7 @@ export interface IButtonContainerProps {
     form?: string;
     key?: string;
     onClick?: any;
+    disabled?: boolean;
 }
 
 export interface IButtonComponentProps extends IButtonContainerProps {}
--- src/components/common/custom-node/add-column-node.tsx
+++ src/components/common/custom-node/add-column-node.tsx
@@ -1,15 +1,72 @@
+import { useState } from 'react';
+import { RiDeleteBinLine } from 'react-icons/ri';
 import { TfiPlus } from 'react-icons/tfi';
+
 import { ITableData } from '../../../store/nodes/types';
+import ButtonContainer from '../button/container';
+import ModalContainer from '../modal/container';
 
 const CustomAddNodeComponent = ({ data }: { data: ITableData }) => {
-    const { addNewNode } = data;
+    const { addNewNode, tableName } = data;
+    const [openModal, setOpenModal] = useState(false);
+    const [deleteEnabled, setDeleteEnabled] = useState(false);
+
     return (
-        <div
-            className="mx-4 flex h-full w-72 justify-center rounded-md border border-dashed border-chelsea-cucumber-500 bg-chelsea-cucumber-100 py-2 text-lg bg-blend-darken hover:border-solid hover:shadow-2xl"
-            onClick={() => addNewNode(data)}
-        >
-            <TfiPlus className="fill-chelsea-cucumber-700 text-xs" />
-        </div>
+        <>
+            <div className="mx-4 flex h-full w-72 justify-center space-x-4">
+                <div
+                    className="w-4/5 rounded-md border border-dashed border-chelsea-cucumber-500 bg-chelsea-cucumber-100 py-2 text-lg bg-blend-darken hover:border-solid hover:shadow-2xl"
+                    onClick={() => addNewNode(data)}
+                >
+                    <TfiPlus className="w-full fill-chelsea-cucumber-700 align-middle text-xs" />
+                </div>
+                <div
+                    className="w-1/5 rounded-md border border-dashed border-chelsea-cucumber-500 bg-chelsea-cucumber-100 py-2 text-lg bg-blend-darken hover:border-solid hover:shadow-2xl"
+                    onClick={() => setOpenModal(true)}
+                >
+                    <RiDeleteBinLine className="w-full fill-chelsea-cucumber-700 align-middle text-xs" />
+                </div>
+            </div>
+            {openModal && (
+                <ModalContainer
+                    open={openModal}
+                    setOpen={setOpenModal}
+                    Header={
+                        <h4 className="flex whitespace-nowrap">
+                            Are you sure you want to delete ?
+                        </h4>
+                    }
+                    Body={
+                        <div className="flex space-x-2">
+                            <input
+                                type={'checkbox'}
+                                checked={deleteEnabled}
+                                onChange={() =>
+                                    setDeleteEnabled(!deleteEnabled)
+                                }
+                            />
+                            <p>
+                                I agree to delete the table{' '}
+                                {tableName.toUpperCase()}
+                            </p>
+                        </div>
+                    }
+                    Footer={
+                        <div className="flex items-center justify-between space-x-4">
+                            <ButtonContainer
+                                label={'Delete'}
+                                onClick={() => {}}
+                                disabled={!deleteEnabled}
+                            />
+                            <ButtonContainer
+                                label={'Cancel'}
+                                onClick={() => setOpenModal(false)}
+                            />
+                        </div>
+                    }
+                />
+            )}
+        </>
     );
 };
 
