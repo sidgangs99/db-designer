8325230277c2d8242b6ad24bb21122a44eab3b4a
Siddharth Gangwar <sidgangs99@gmail.com>
2023-03-23 23:57:53 +0530

    feat(sidebar): add download image option

---
+++ src/components/common/downloadHTML/component.tsx
@@ -0,0 +1,44 @@
+import { toPng } from 'html-to-image';
+import { RiCamera3Fill } from 'react-icons/ri';
+
+function downloadImage(dataUrl: string) {
+    const a = document.createElement('a');
+
+    a.setAttribute('download', 'db-cult.png');
+    a.setAttribute('href', dataUrl);
+    a.click();
+}
+
+function DownloadButton() {
+    const onClick = () => {
+        const reactFlowEl = document.querySelector(
+            '.react-flow'
+        ) as HTMLElement;
+        if (!reactFlowEl) {
+            console.error('React Flow element not found.');
+            return;
+        }
+
+        toPng(reactFlowEl, {
+            filter: (node) => {
+                // we don't want to add the minimap and the controls to the image
+                if (
+                    node?.classList?.contains('react-flow__minimap') ||
+                    node?.classList?.contains('react-flow__controls')
+                ) {
+                    return false;
+                }
+
+                return true;
+            }
+        }).then(downloadImage);
+    };
+
+    return (
+        <div onClick={onClick}>
+            <RiCamera3Fill className=" m-2 fill-chelsea-cucumber-500 text-lg" />
+        </div>
+    );
+}
+
+export default DownloadButton;
