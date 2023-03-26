3c35b007745bfece1019aa3915a06442b8171067
Siddharth Gangwar <siddharth_gangwar@cargill.com>
2023-03-26 19:51:37 +0530

    chore(workbook): remove initial nodes & edges

--- src/components/react-flow/constants.ts
+++ src/components/react-flow/constants.ts
@@ -3,6 +3,7 @@ import CustomColumnNodeComponent from '../common/custom-node/column-node';
 import CustomTableNodeComponent from '../common/custom-node/table-node';
 import { ECustomNodeTypes } from './types';
 
+
 export const nodeTypes = {
     [ECustomNodeTypes.TableNode]: CustomTableNodeComponent,
     [ECustomNodeTypes.ColumnNode]: CustomColumnNodeComponent,
